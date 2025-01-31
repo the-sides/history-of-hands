f8749ddb1772deaf49b1ca5e01379317ce7bd39e
Jacob Sides <sides798@gmail.com>
2025-01-30 22:45:11 -0500

    Dayjs for current time

--- src/app/_components/gameStage.tsx
+++ src/app/_components/gameStage.tsx
@@ -1,11 +1,16 @@
 
+import dayjs from "dayjs";
 import type { LoadedGame } from "../models/game";
 import CardSlider from "./cardSlider";
 
+const rn = dayjs()
 export default function GameStage({ game }: { game: LoadedGame }) {
     return (
         <div className="flex flex-1 flex-col justify-center items-center text-3xl md:text-5xl w-[calc(100vw-20px)]">
             <h2 className="mt-12">Save a New Round</h2>
+            <h4 className="text-4xl mt-4">{rn.format('MMM DD')}</h4>
+            <h5 className="text-2xl">{rn.format('h:mm a')}</h5>
+            
 
             <div className="flex justify-between w-full overflow-hidden">
                 <CardSlider user={game.createdByUser} isOwner />
