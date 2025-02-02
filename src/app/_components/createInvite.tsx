bcbcc2e1d758a70e2baa297cd30ca21a18249f57
the-sides <jacob.r.sides@gmail.com>
2025-02-02 00:07:50 -0500

    Copy invite link to clipboard

--- src/app/_components/createInvite.tsx
+++ src/app/_components/createInvite.tsx
@@ -1,23 +1,36 @@
 'use client';
 
-import Link from "next/link";
 import { useState } from "react";
+import { toast } from "sonner";
 import { api } from "~/trpc/react"
 
 export default function CreateInvite({ gameId }: { gameId: number }) {
-
     const [link, setLink] = useState('');
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(false);
+    const [copied, setCopied] = useState(false);
 
     const createInvite = api.game.createInvite.useMutation({
         onSuccess(data) {
-            console.log(data)
-            if (data?.id) setLink('history-of-hands.vercel.app/accept-invite/' + data.id)
+            if (data?.id) {
+                const fullLink = 'https://history-of-hands.vercel.app/accept-invite/' + data.id;
+                setLink(fullLink);
+                navigator.clipboard.writeText(fullLink);
+                toast.success('Copied invite link to clipboard!');
+                // setCopied(true);
+                // setTimeout(() => setCopied(false), 2000); // Reset copied state after 2 seconds
+            }
         }
     })
-    if (link) return <Link prefetch={false} href={'https://'+link} className="break-all underline">{link}</Link>
-    return <button onClick={() => {
-        setLoading(true)
-        createInvite.mutate({ gameId })
-    }} className={`underline`}>{loading ? 'Loading Link...' : 'Copy Invite Link'}</button>
+
+    if (link) return <span className="break-all">{copied ? 'Copied!' : link}</span>;
+    
+    return <button 
+        onClick={() => {
+            setLoading(true);
+            createInvite.mutate({ gameId });
+        }} 
+        className="underline"
+    >
+        {loading ? 'Loading Link...' : 'Copy Invite Link'}
+    </button>
 }
