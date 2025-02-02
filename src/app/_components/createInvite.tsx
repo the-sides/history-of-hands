a854f89c3a68f1c45669f4b9e74386d06bfe81d0
the-sides <jacob.r.sides@gmail.com>
2025-02-02 00:13:36 -0500

    more lints

--- src/app/_components/createInvite.tsx
+++ src/app/_components/createInvite.tsx
@@ -7,22 +7,19 @@ import { api } from "~/trpc/react"
 export default function CreateInvite({ gameId }: { gameId: number }) {
     const [link, setLink] = useState('');
     const [loading, setLoading] = useState(false);
-    const [copied, setCopied] = useState(false);
 
     const createInvite = api.game.createInvite.useMutation({
-        onSuccess(data) {
+        async onSuccess(data) {
             if (data?.id) {
                 const fullLink = 'https://history-of-hands.vercel.app/accept-invite/' + data.id;
                 setLink(fullLink);
-                navigator.clipboard.writeText(fullLink);
+                await navigator.clipboard.writeText(fullLink);
                 toast.success('Copied invite link to clipboard!');
-                // setCopied(true);
-                // setTimeout(() => setCopied(false), 2000); // Reset copied state after 2 seconds
             }
         }
     })
 
-    if (link) return <span className="break-all">{copied ? 'Copied!' : link}</span>;
+    if (link) return <span className="break-all">{link}</span>;
     
     return <button 
         onClick={() => {
